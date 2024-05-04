0859f8b0f2b32e8f441f6582040884b157c98319
Chris Michael <chris.perez@upr.edu>
2024-05-04 13:17:04 -0400

    define tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,55 @@
 import { describe, expect, it } from 'vitest'
+import { size } from 'lodash'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+import api from '../dist'
+
+describe('scraper', () => {
+  it('should return the latest added anime', async () => {
+    const result = await api.latestAnimeAdded()
+    expect(result).toBeDefined()
+    expect(size(result)).toBeGreaterThan(0)
+  })
+
+  it('should return filtered anime based on criteria', async () => {
+    const result = await api.filter({ query: { genre: 'sci-fi', year: '2024' } })
+    expect(result).toBeDefined()
+    expect(size(result)).toBeGreaterThan(0)
+  })
+
+  it('should return anime by alphabet', async () => {
+    const result = await api.byAlphabet('A')
+    expect(result).toBeDefined()
+    expect(size(result)).toBeGreaterThan(0)
+  })
+
+  it('should return extra information about a specific anime', async () => {
+    const result = await api.getExtraInfo('tokyo-ghoul')
+    expect(result).toBeDefined()
+    expect(result).toHaveProperty('extra')
+  })
+
+  it('should return top anime for a specific season and year', async () => {
+    const result = await api.top('Invierno', '2020')
+    expect(result).toBeDefined()
+    expect(size(result)).toBeGreaterThan(0)
+  })
+
+  it('should return the anime schedule', async () => {
+    const result = await api.schedule()
+    expect(result).toBeDefined()
+    expect(size(result)).toBeGreaterThan(0)
+  })
+
+  it('should return search results for a query', async () => {
+    const result = await api.search('tokyo ghoul')
+    expect(result).toBeDefined()
+    if (result)
+      expect(size(result.animes)).toBeGreaterThan(0)
+  })
+
+  it('should return servers for streaming an anime', async () => {
+    const result = await api.getAnimeServers('tokyo-ghoulre', 1)
+    expect(result).toBeDefined()
+    expect(size(result)).toBeGreaterThan(0)
   })
 })
